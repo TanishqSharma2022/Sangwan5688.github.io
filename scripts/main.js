9ae21bbb0356fefcbd776598dda79f1275c9a85f
Ankit Sangwan <ankit.sa@browserstack.com>
2023-01-29 23:50:21 +0530

    added image animation

--- scripts/main.js
+++ scripts/main.js
@@ -76,6 +76,19 @@ class TxtType {
   }
 }
 
+let scroll = window.requestAnimationFrame || function(callback) { window.setTimeout(callback, 1000 / 60) };
+let elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+  elementsToShow.forEach(function(element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+  scroll(loop);
+}
 
 window.onload = function() {
   let elements = document.getElementsByClassName('typewrite');
@@ -85,4 +98,20 @@ window.onload = function() {
       new TxtType(elements[i], JSON.parse(toRotate), 1500);
     }
   }
+  loop();
 };
+
+function isElementInViewport(el) {
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 &&
+      rect.bottom >= 0) ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
