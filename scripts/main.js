4e1850206b9de01c16386a1f198815c9c8b8aebb
Ankit Sangwan <ankit.sa@browserstack.com>
2023-02-19 22:50:13 +0530

    changed image slider

--- scripts/main.js
+++ scripts/main.js
@@ -152,3 +152,50 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
+
+jQuery(document).ready(function() {
+
+  function detect_active() {
+    // get active
+    var get_active = $("#dp-slider .dp_item:first-child").data("class");
+    $("#dp-dots li").removeClass("active");
+    $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
+  }
+  $("#dp-next").click(function() {
+    $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function(index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+    detect_active();
+  });
+
+  $("#dp-prev").click(function() {
+    $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function(index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+    detect_active();
+  });
+
+  $("#dp-dots li").click(function() {
+    $("#dp-dots li").removeClass("active");
+    $(this).addClass("active");
+    let get_slide = $(this).attr('data-class');
+    console.log(get_slide);
+    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function(index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+  });
+
+
+  $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function() {
+    var get_slide = $(this).attr('data-class');
+    console.log(get_slide);
+    $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+    $.each($('.dp_item'), function(index, dp_item) {
+      $(dp_item).attr('data-position', index + 1);
+    });
+    detect_active();
+  });
+});
